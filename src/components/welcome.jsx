2837f621a768aeb2ca18c696af7f14870b977b80
Chemige <o.marchenko.breneur@outlook.com>
2024-08-29 15:19:59 +0200

    links

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -9,7 +9,9 @@ const WelcomePage = ({setMode}) => (
             <h2>Rules:</h2>
             <ul className="list">
                 <li>In <strong>Training Mode</strong>, questions are split into many small subtopics and are not mixed.<br/></li>
-                <li>In <strong>Test Mode</strong>, questions cover whole topics and are shuffled. You will have 20 minutes solve 15 questions.</li>
+                <li>In <strong>Test Mode</strong>, questions cover whole topics and are shuffled. You will have 20 minutes solve 15 questions.<br/></li>
+                <li>If you have found a bug or have an idea on how to improve the app, feel free to contact me at <a href="mailto: oleksandr.marchenko.002@student.uni.lu">oleksandr.marchenko.002@student.uni.lu</a><br/></li>
+                <li>Finally, feel free to contribute to the GitHub repository <a href="https://github.com/Oleksandr-MB/quiz">https://github.com/Oleksandr-MB/quiz</a></li>
             </ul>
             <h2>Good luck!</h2>
         </div>
