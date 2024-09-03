72a1dc1a3cb75ecfc8b69ef7c2525a58b6706a17
Chemige <o.marchenko.breneur@outlook.com>
2024-09-03 21:54:23 +0200

    egrwef

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -11,8 +11,6 @@ const WelcomePage = ({setMode}) => (
                 <li>In <strong>Training Mode</strong>, questions are split into many small subtopics and are not mixed.<br/></li> 
                 <li>In <strong>Test Mode</strong>, questions cover whole topics and are shuffled. You will have 20 minutes to solve 15 questions.</li>
                 <li>Fully correct answers give you 1 point, partially correct - 0.5 points and wrong answers - 0 points.<br/></li> 
-                <li>If you encounter a bug or have an idea for improvement, please contact me at <a className="link" href="mailto:oleksandr.marchenko.002@student.uni.lu">oleksandr.marchenko.002@student.uni.lu</a><br/></li> 
-                <li>Contributions to the GitHub repository are welcome: <a className="link" href="https://github.com/Oleksandr-MB/quiz">https://github.com/Oleksandr-MB/quiz</a></li>
             </ul>
             <h2>Good luck!</h2>
         </div>
