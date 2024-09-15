dea20f004a5938dfb05654da857cd250be147089
Chemige <o.marchenko.breneur@outlook.com>
2024-09-15 03:12:28 +0200

    bcnv

--- src/components/train/quiz.jsx
+++ src/components/train/quiz.jsx
@@ -90,15 +90,19 @@ const Quiz = ({ setMode, filteredQuestions, setFilteredQuestions, subTopic, setS
                 <div className="popup">
                     <div className="popup-content">
                         <div className="popup-feedback">
-                            <span className="question-recap">{feedback.status}</span><br/>
-                            <span className="answer-recap">You selected: </span><Latex>{String(feedback.userSelection)}</Latex><br/>
-                            <span className="answer-recap">Correct answer: </span><Latex>{String(feedback.correctAnswers)}</Latex><br/>
+                            <p className="result">
+                                <span className="question-recap">{feedback.status}</span><br/>
+                                <span className="answer-recap">You selected: </span><Latex>{String(feedback.userSelection)}</Latex><br/>
+                                <span className="answer-recap">Correct answer: </span><Latex>{String(feedback.correctAnswers)}</Latex><br/>
+                            </p>
+
                             <div className="show-solution-btn">
                                 <span onClick={() => setShowSolution(!showSolution)}>Show solution {showSolution ? "\u2227" : "\u2228"}</span>
                             </div>
                             <div className="show-solution" hidden={!showSolution}>
                                 <span className="answer-recap">Solution: </span><Latex>{String(feedback.solution)}</Latex><br/>
                             </div>
+
                             <div className="footer">
                                 <button className="btn-next" onClick={() => {setShowSolution(false); closeResultPopUp()}}>Next question</button>
                             </div>
--- src/components/train/result.jsx
+++ src/components/train/result.jsx
@@ -7,12 +7,12 @@ const Result = ({filteredQuestions, setMode, setState}) => {
             <div className="header"><h1 className="header-text">Recap</h1></div>
             <div className="content">
                 <ol className="recap">
-                    {filteredQuestions.map((question, index) => ( 
+                    { filteredQuestions.map((question, index) => ( 
                         <>
                             <li key={index}>
-                                <span className="question-recap"><Latex>{question.text}</Latex></span><br/>
-                                <span className="answer-recap">Correct answer: </span><Latex>{question.correctAnswer.join(", ")}</Latex><br/>
-                                <span className="answer-recap" hidden={!question.solution}>Solution: </span><Latex>{question.solution}</Latex><br/>
+                                <span className="question-recap"><Latex>{String(question.text)}</Latex></span><br/>
+                                <span className="answer-recap">Correct answer: </span><Latex>{String(question.correctAnswer.join(", "))}</Latex><br/>
+                                <span className="answer-recap" hidden={!question.solution}>Solution: </span><Latex>{String(question.solution)}</Latex><br/>
                             </li>
                             <div className="line"></div>
                         </>
